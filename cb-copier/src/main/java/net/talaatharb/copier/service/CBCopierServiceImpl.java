3155ad5f5de81cae1f8bed5ae2d05bbae6715e23
mharb <mohamed.harb@cegedim.com>
2025-05-04 18:47:23 +0300

    fix some null pointer issues, and upgraded spring version

--- cb-copier/src/main/java/net/talaatharb/copier/service/CBCopierServiceImpl.java
+++ cb-copier/src/main/java/net/talaatharb/copier/service/CBCopierServiceImpl.java
@@ -20,85 +20,94 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CBCopierServiceImpl implements CBCopierService {
 
-	private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-	@Override
-	public boolean deleteWithFilter(CouchbaseTemplate template, String filter) {
-		final QueryResult result = template.getCouchbaseClientFactory().getCluster()
-				.query("DELETE FROM " + template.getBucketName() + " WHERE " + filter);
-		return result != null;
-	}
+    @Override
+    public boolean deleteWithFilter(CouchbaseTemplate template, String filter) {
+        final QueryResult result = template.getCouchbaseClientFactory().getCluster()
+                .query("DELETE FROM " + template.getBucketName() + " WHERE " + filter);
+        return result != null;
+    }
 
-	@Override
-	public String fetchUsingQuery(CouchbaseTemplate template, String query) {
-		final QueryResult result = template.getCouchbaseClientFactory().getCluster().query(query);
+    @Override
+    public String fetchUsingQuery(CouchbaseTemplate template, String query) {
+        final QueryResult result = template.getCouchbaseClientFactory().getCluster().query(query);
 
-		// Assuming the result is not null and has content
-		if (result != null && !result.rowsAsObject().isEmpty()) {
-			// Convert the result to a String for simplicity
-			return result.rowsAsObject().toString();
-		}
+        // Assuming the result is not null and has content
+        if (result != null && !result.rowsAsObject().isEmpty()) {
+            // Convert the result to a String for simplicity
+            return result.rowsAsObject().toString();
+        }
 
-		return "{\"result\":\"No results\"}";
-	}
+        return "{\"result\":\"No results\"}";
+    }
 
-	@Override
-	public boolean insert(CouchbaseTemplate template, JsonNode data, String oldBucket) {
-		if (data instanceof final ArrayNode arrayData) {
-			final List<String> values = StreamSupport
-					.stream(Spliterators.spliterator(arrayData.elements(), arrayData.size(), Spliterator.ORDERED),
-							false)
-					.map(element -> element.findValue(oldBucket)).map(this::mapToEntry).filter(e -> e != null)
-					.map(entry -> String.format("('%s', %s)", entry.getKey(), entry.getValue())).toList();
+    @Override
+    public boolean insert(CouchbaseTemplate template, JsonNode data, String oldBucket) {
+        if (data instanceof final ArrayNode arrayData) {
+            final List<String> values = StreamSupport
+                    .stream(Spliterators.spliterator(arrayData.elements(), arrayData.size(), Spliterator.ORDERED),
+                            false)
+                    .map(element -> element.findValue(oldBucket)).map(this::mapToEntry).filter(e -> e != null)
+                    .map(entry -> String.format("('%s', %s)", entry.getKey(), entry.getValue())).toList();
 
-			final int count = values.size();
-			final StringBuilder builder = new StringBuilder("INSERT INTO ");
-			builder.append(template.getBucketName());
-			builder.append(" (KEY,VALUE) \n VALUES ");
-			for (int i = 0; i < count - 1; i++) {
-				builder.append(values.get(i));
-				builder.append(",");
-			}
-			builder.append(values.get(count - 1));
-			builder.append(";");
-			template.getCouchbaseClientFactory().getCluster().query(builder.toString());
-			return true;
-		}
-		return false;
-	}
+            final int count = values.size();
+            final StringBuilder builder = new StringBuilder("INSERT INTO ");
+            builder.append(template.getBucketName());
+            builder.append(" (KEY,VALUE) \n VALUES ");
+            for (int i = 0; i < count - 1; i++) {
+                builder.append(values.get(i));
+                builder.append(",");
+            }
+            builder.append(values.get(count - 1));
+            builder.append(";");
+            template.getCouchbaseClientFactory().getCluster().query(builder.toString());
+            return true;
+        }
+        return false;
+    }
 
-	private Entry<String, String> mapToEntry(JsonNode v) {
-		try {
-			return new AbstractMap.SimpleImmutableEntry<>(v.findValue("id").asText(),
-					objectMapper.writeValueAsString(v));
-		} catch (final JsonProcessingException e) {
-			return null;
-		}
-	}
+    private Entry<String, String> mapToEntry(JsonNode v) {
+        if (v == null) {
+            return null;
+        }
+        String id = v.findValue("id").asText();
+        if(id == null) {
+            return null;
+        }
+        try {
+            return new AbstractMap.SimpleImmutableEntry<>(id,
+                    objectMapper.writeValueAsString(v));
+        } catch (final JsonProcessingException e) {
+            return null;
+        }
+    }
 
-	@Override
-	public boolean upsert(CouchbaseTemplate template, JsonNode data, String oldBucket) {
-		if (data instanceof final ArrayNode arrayData) {
-			final List<String> values = StreamSupport
-					.stream(Spliterators.spliterator(arrayData.elements(), arrayData.size(), Spliterator.ORDERED),
-							false)
-					.map(element -> element.findValue(oldBucket)).map(this::mapToEntry).filter(e -> e != null)
-					.map(entry -> String.format("('%s', %s)", entry.getKey(), entry.getValue())).toList();
+    @Override
+    public boolean upsert(CouchbaseTemplate template, JsonNode data, String oldBucket) {
+        if (data instanceof final ArrayNode arrayData) {
+            final List<String> values = StreamSupport
+                    .stream(Spliterators.spliterator(arrayData.elements(), arrayData.size(), Spliterator.ORDERED),
+                            false)
+                    .map(element -> element.findValue(oldBucket)).map(this::mapToEntry).filter(e -> e != null)
+                    .map(entry -> String.format("('%s', %s)", entry.getKey(), entry.getValue())).toList();
 
-			final int count = values.size();
-			final StringBuilder builder = new StringBuilder("UPSERT INTO ");
-			builder.append(template.getBucketName());
-			builder.append(" (KEY,VALUE) \n VALUES ");
-			for (int i = 0; i < count - 1; i++) {
-				builder.append(values.get(i));
-				builder.append(",");
-			}
-			builder.append(values.get(count - 1));
-			builder.append(";");
-			template.getCouchbaseClientFactory().getCluster().query(builder.toString());
-			return true;
-		}
-		return false;
-	}
+            if (!values.isEmpty()) {
+                final int count = values.size();
+                final StringBuilder builder = new StringBuilder("UPSERT INTO ");
+                builder.append(template.getBucketName());
+                builder.append(" (KEY,VALUE) \n VALUES ");
+                for (int i = 0; i < count - 1; i++) {
+                    builder.append(values.get(i));
+                    builder.append(",");
+                }
+                builder.append(values.get(count - 1));
+                builder.append(";");
+                template.getCouchbaseClientFactory().getCluster().query(builder.toString());
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
